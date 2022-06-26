092b7f09da51f2f681d449e8de40673fd297128a
Wibubot-pricing <Stewardjason10@Gmail.com>
2022-06-26 14:31:09 +0700

    Create Affiliate.php

---
+++ Affiliate.php
@@ -0,0 +1,22 @@
+//jangan mengganti apapun
+
+<button>mulai affiliate</button>
+<bot>progressing</bot>
+
+<a href "bot sedang menjalankan kode affiliates partner by Wibubot"
+{ 
+   "fake Whatsapp Chatroom":'Wibubot Chatroom'
+   "Whatsapp-green-badge.jpeg":'verified√'
+   "Notes":'Whatsapp mengamankan chat anda dengan wibubot silahkan ketuk selengkapnya'
+   "Reply":'Silahkan ketik disini'
+   "Chatbox":'360×1200×750'
+   "chatbot chat":'${reply teks}'
+   "Autoreply":'on'
+   "autoread":'on'
+}
+{
+   //Autorespond Online Fake Bot
+     "Teks":'akhiri'
+       "jawaban":'baik kami akan mengakhiri chat ini ${out}'
+}
+//terima kasih
